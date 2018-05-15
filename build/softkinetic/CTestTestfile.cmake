8ddda5b7343597ab8feeaf1b35459613fe3d1a97
Sapta <saptadeep.deb@gmail.com>
2018-05-15 11:45:30 +0200

    chnges

--- build/softkinetic/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fdai5587/ws/src/softkinetic
-# Build directory: /home/fdai5587/ws/build/softkinetic
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/softkinetic_camera/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fdai5587/ws/src/softkinetic_camera
-# Build directory: /home/fdai5587/ws/build/softkinetic_camera
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
